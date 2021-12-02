77d28f15527e2148eedde6720cb479509d15c5d8
Apinop <apinop.soisuwan@gmail.com>
2021-12-03 05:38:03 +0700

    ** need to fix bug ( Blank input )

--- Rock, paper, scissors Python Project.py
+++ Rock, paper, scissors Python Project.py
@@ -7,19 +7,30 @@ def RPS_Rule ():
     #Player
     def Player():
         player = {1:"Rock",2:"Paper",3:"Scissors"}
-        player_choice = input("Select Rock = 1, Paper = 2 , Scissors = 3 \nSelect: ")
+        player_choice = input("\nSelect Rock = 1, Paper = 2 , Scissors = 3 \nSelect: ")
         coin_play = 0
         while coin_play <= 1:
-            if int(player_choice) >= 4 :
+            if int(player_choice) >= 4 or int(player_choice) <= 0:
                 print("Error:Try again")
                 return Player()
             elif int(player_choice) <= 3 :
                 coin_play += 1
                 return player[int(player_choice)]
 
+    def play_again (n):
+        coin_press = 0
+        while coin_press <= 1:
+            if n == 1:
+                coin_press += 1
+                return(RPS_Rule())
+            elif n == 0:
+                coin_press +=1
+                return("\nEXIT: ___Thank you___")
+            else:
+                return play_again(int(input("\nError: Play Again Press 1 , Exit Press 0\nPress: ")))
+
     player = Player()
     bot = Bot()
-
     # Rule
     Coin_rule = 0
     while Coin_rule < 1 :
@@ -31,14 +42,15 @@ def RPS_Rule ():
                 player == "Rock" and bot == "Scissors" \
                 or player == "Scissors" and bot == "Paper":
             Coin_rule += 1
-            print("Player: "+ player + "\n" +"Com:    " + bot +"\nPlayer Win \nPlay Again Press 1 , Exit Press 0")
+            print("\nPlayer: "+ player + "\n" +"Com:    " + bot +"\nPlayer Win \nPlay Again Press 1 , Exit Press 0")
         #Bot Win
         elif bot == "Paper" and player == "Rock" or \
                 bot == "Rock" and player == "Scissors" \
                 or bot == "Scissors" and player == "Paper":
             Coin_rule += 1
-            print("Player: "+ player + "\n" +"Com:    " + bot +"\nBot Win \nPlay Again Press 1 , Exit Press 0")
+            print("\nPlayer: "+ player + "\n" +"Com:    " + bot +"\nBot Win \nPlay Again Press 1 , Exit Press 0")
 
+    return play_again(int(input("\nPress: ")))
 
 print(RPS_Rule())
 
